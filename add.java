e5d0ea7e12aadf68a76c874b97137b9dc3eeccd8
Shubhaprasad Padhy <rupelpadhy@gmail.com>
2020-10-02 11:06:30 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,10 +1,14 @@
 import java.util.*;
 
 public class add{
+    
+    public static int sum(int x , int y){
+        return x+y;
+    }
     public static void main(String [] args){
         Scanner sc = new Scanner (System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a+b);
+        System.out.println(sum(a,b));
     }
 }
